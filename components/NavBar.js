2583a6b8092faded4514076447c22dc480986072
lostmart <lostmart@hotmail.com>
2022-06-21 18:47:32 +0200

    a nice comment goes here ...

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,8 +4,8 @@ class NavBar extends HTMLElement {
 		this.innerHTML = `
             <nav class="navbar navbar-expand-lg navbar-light bg-primary">
 				<div class="container-fluid">
-					<a class="navbar-brand text-white" href="#">
-						<img src="./favicon-32x32.png" alt="" />
+					<a class="navbar-brand text-white" href="/">
+						<img src="./favicon-32x32.png" alt="padrinos sin limites" />
 					</a>
 					<button
 						class="navbar-toggler"
@@ -32,7 +32,7 @@ class NavBar extends HTMLElement {
 								<a class="nav-link text-white" href="./about.html">QUIÉNES SOMOS</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link text-white" href="#">PREGUNTAS</a>
+								<a class="nav-link text-white" href="./faq.html">PREGUNTAS</a>
 							</li>
 							<li class="nav-item">
 								<a class="nav-link text-white" href="#">CONTACTO</a>
